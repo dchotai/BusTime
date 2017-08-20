e7ad6c3439a588d4c9033c1a64ad0a7eecdec94e
Divyesh Chotai <dchotai@users.noreply.github.com>
2017-08-19 23:21:41 -0700

    Initial commit

---
+++ BusTime/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BusTime
+//
+//  Created by Divyesh Chotai on 8/18/17.
+//  Copyright © 2017 Divyesh Chotai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BusTime/MenuController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuController.swift
+//  BusTime
+//
+//  Created by Divyesh Chotai on 8/19/17.
+//  Copyright © 2017 Divyesh Chotai. All rights reserved.
+//
+
+import Cocoa
+
+class MenuController: NSObject {
+
+    @IBOutlet weak var window: NSWindow!
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    override func awakeFromNib() {
+        let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+    }
+
+}
