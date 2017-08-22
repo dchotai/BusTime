8eb0cc91b667f6e6d0f039b1271384df087aa98a
Divyesh Chotai <dchotai@users.noreply.github.com>
2017-08-22 13:35:12 -0700

    Added route directions

--- BusTime/ACTransitAPI.swift
+++ BusTime/ACTransitAPI.swift
@@ -12,6 +12,8 @@ class ACTransitAPI {
     let TOKEN = "?token=E8B5BCE12863B6FF772C0D59FB2843AF" //token from demo
     let BASEURL = "http://api.actransit.org/transit/"
     
+    static var routeIDs = [String]()
+    
     func getScheduleType() -> Int? {
         let day = Calendar.current.component(.weekday, from: Date())
         
@@ -35,7 +37,8 @@ class ACTransitAPI {
             if let httpResponse = response as? HTTPURLResponse {
                 if (httpResponse.statusCode == 200){
                     if let routes = self.parseRouteIDs(data!) {
-                        NSLog("\(routes)")
+//                        NSLog("\(routes)")
+                        ACTransitAPI.routeIDs = routes
                     }
                 } else {
                     NSLog("API Error: %d %@", httpResponse.statusCode, HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))
@@ -61,7 +64,54 @@ class ACTransitAPI {
             let routeID = item["RouteId"] as! String
             routes.append(routeID)
         }
-        
         return routes
     }
+    
+    func getRouteDirections(routeID: String) -> [String]? {
+        let session = URLSession.shared
+        let url = URL(string: "\(BASEURL)route/\(routeID)/directions/\(TOKEN)")
+        
+        var rv = [String]()
+        let task = session.dataTask(with: url!) { data, response, error in
+            if let e = error {
+                NSLog("API Error: \(e)")
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                if (httpResponse.statusCode == 200){
+                    if let directions = self.parseRouteDirections(data!) {
+                        NSLog("\(directions)")
+                        rv = directions
+                    }
+                } else {
+                    NSLog("API Error: %d %@", httpResponse.statusCode, HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))
+                }
+            }
+        }
+        task.resume()
+        return rv
+    }
+    
+    func parseRouteDirections(_ data: Data) -> [String]? {
+        let json: [String]
+        
+        do {
+            json = try JSONSerialization.jsonObject(with: data) as! [String]
+        } catch {
+            NSLog("Direction parsing error")
+            return nil
+        }
+        
+        var directions = [String]()
+        
+        let count = json.count
+        var i = 1
+        
+        while (i < count) {
+//            NSLog(json[i] as! String)
+            directions.append(json[i])
+            i += 2
+        }
+        return directions
+    }
+    
 }
