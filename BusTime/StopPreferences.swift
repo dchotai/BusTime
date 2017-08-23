b8c657a0de10ad4a4e9b5b2534f48318d4ca8c9c
Divyesh Chotai <dchotai@users.noreply.github.com>
2017-08-23 16:42:16 -0700

    Populate routes and directions for new item

--- BusTime/StopPreferences.swift
+++ BusTime/StopPreferences.swift
@@ -11,12 +11,26 @@ import Cocoa
 class StopPreferences: NSWindowController {
     
     @IBOutlet weak var segmentedControl: NSSegmentedControl!
+    
     @IBOutlet var prefWindow: NSWindow!
     
     @IBOutlet var addStop: NSWindow!
+    
+    @IBOutlet weak var routePopup: NSPopUpButton!
+    
+    @IBOutlet weak var directionPopup: NSPopUpButton!
+    
+    @IBOutlet weak var stopPopup: NSPopUpButton!
+    
+    weak var api:ACTransitAPI!
+    
     @IBAction func segmentedControlAction(_ sender: Any) {
         if (segmentedControl.selectedSegment == 0) {
-            NSLog("plus")
+//            NSLog("plus")
+            
+            routePopup.removeAllItems()
+            routePopup.addItems(withTitles: ACTransitAPI.routeIDs)
+            
             prefWindow.beginSheet(addStop, completionHandler: nil)
             
 //            prefWindow.endSheet(addStop)
@@ -26,6 +40,33 @@ class StopPreferences: NSWindowController {
             NSLog("edit")
         }
     }
+    
+    @IBAction func routeSelect(_ sender: Any) {
+        let index = routePopup.indexOfSelectedItem
+        
+        directionPopup.removeAllItems()
+        
+        var id : String
+        id = ACTransitAPI.routeIDs[index]
+        
+        api.getRouteDirections(routeID: id) {
+            (_ result) -> Void in
+            self.directionPopup.addItems(withTitles: result)
+        }
+    }
+    
+    
+    @IBAction func directionSelect(_ sender: Any) {
+
+        
+        
+    }
+    
+    
+    @IBAction func cancelAddStop(_ sender: Any) {
+        prefWindow.endSheet(addStop)
+    }
+    
     
     override var windowNibName : String! {
         return "StopPreferences"
