4e60f66446b3c91287b0c681d51121e559c86f01
Divyesh Chotai <dchotaidev@gmail.com>
2017-08-27 17:28:54 -0700

    Added stop prediction times

--- BusTime/MenuController.swift
+++ BusTime/MenuController.swift
@@ -32,11 +32,7 @@ class MenuController: NSObject {
         stopPrefs = StopPreferences()
         stopPrefs.setMenuController(mc: self)
         api.getRoutes()
-        
-        
-//        defaults.removeObject(forKey: "savedStopNames")
-//        defaults.removeObject(forKey: "savedStopIDs")
-//        defaults.removeObject(forKey: "savedStopRoutes")
+        updateStops()
     }
     
     @IBAction func prefClicked(_ sender: NSMenuItem) {
@@ -49,8 +45,6 @@ class MenuController: NSObject {
         let savedStopIDs = defaults.object(forKey: "savedStopIDs") as? [String:Int] ?? [String:Int]()
         let savedStopRoutes = defaults.object(forKey: "savedStopRoutes") as? [String:String] ?? [String:String]()
         
-        
-        
         let keep = Array(statusMenu.items.suffix(3))
         statusMenu.removeAllItems()
         for item in keep {
@@ -59,13 +53,11 @@ class MenuController: NSObject {
         
         if (savedStopNames.count > 0) {
             for name in savedStopNames {
-                
-                api.getPrediction(stopID: savedStopIDs[name]!) {
+                api.getPrediction(stopID: savedStopIDs[name]!, routeID: savedStopRoutes[name]!) {
                     (_ rv) -> Void in
                     let newitem = NSMenuItem()
                     let sv = StopView(frame: NSRect(x:0, y:0, width:175, height:40))
                     sv.updateTime(stop: name, time: rv, route: "\(savedStopRoutes[name]!)")
-                    
                     newitem.view = sv
                     self.statusMenu.insertItem(NSMenuItem.separator(), at: 0)
                     self.statusMenu.insertItem(newitem, at: 0)
@@ -73,7 +65,6 @@ class MenuController: NSObject {
                 
             }
         }
-
     }
     
 }
